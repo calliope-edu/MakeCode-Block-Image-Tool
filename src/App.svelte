af64a19fd7c333fee61ada9d24ec29092a499f07
JW <gitkraken@juriwolf.de>
2022-03-25 19:49:02 +0100

    Cleanup code and add microbit as image source

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <main class="container">
     <hgroup>
-        <h1>MakeCode Block Images</h1>
+        <h1>MakeCode Block Bilder für
+            <select id="fruit" required style='display:inline-block; width:auto' bind:value={editorURL}>
+              <option value='https://makecode.calliope.cc/' selected>Calliope</option>
+              <option value='https://makecode.microbit.org/'>Microbit</option>
+            </select>
+        </h1>
         <h2>Mit diesem Tool können die Blöcke aus MakeCode anhand von Code als Bilder in SVG und als PNG erstellt werden.</h2>
     </hgroup>
 
@@ -25,15 +30,15 @@
 
             <label for="projectUrl">
                 MakeCode Projekt Link (Optional)
-                <input type="text" name="projectUrl" bind:value={packageIdStrURL} />
+                <input type="text" name="projectUrl" placeholder="https://makecode.calliope.cc/..." bind:value={packageIdStrURL} />
                 <small>MakeCode Projekt-URL über die Teilen funktion erstellen und hier eingeben, um dort geladene Erweiterungen verwenden zu können oder das ganze Projekt zu Rendern.</small>
             </label>
     
         </div>
   
         <label for="code">
-            Code:
-            <textarea name="code" bind:value={code}></textarea>
+            Code
+            <textarea name="code" bind:value={code} placeholder="basic.showIcon(IconNames.Heart) ..."></textarea>
             <small>Leer lassen und oben einen MakeCode Projekt Link angeben, um ein ganzes projekt zu rendern.</small>
         </label>
   
@@ -45,19 +50,20 @@
         <a download="bild.png" bind:this={pngLink}><button>PNG Herunterladen</button></a>
     </div>
 
-
     <div class="item codeImage">
         <div class="grid">
-            <div class="image-container"></div>
+            <div class="image-container" bind:this={svgContainer}></div>
             <div class="image-containerPNG" bind:this={pngContainer}></div>
         </div>
 	</div>
 
 </main>
 
-<pre class="makecodeSnippet lds-dual-ring" id="snippet_1">
+<pre class="makecodeSnippet lds-dual-ring" id="snippet_1" bind:this={codePre}>
     {code}
 </pre>
+
+<iframe bind:this={iframeHandler} src='{editorURL}--docs?render=1' style="position: absolute; left: 0; bottom: 0; width: 1px; height: 1px; border: none;"></iframe>
 
 <ThemeToggle />
 
@@ -65,13 +71,17 @@
 
 	textarea {
 		width: 100%;
-		height: 10em;
+		height: 12em;
         font-family: monospace;
 	}
 
 	.makecodeSnippet {
 		display: none;
 	}
+
+    ::placeholder {
+        opacity: .3;
+    }
 
 </style>
 
@@ -81,59 +91,36 @@
         
     let code = ``; // code to be rendered as png
     $: codeToRender = (code.length < 2) ? "" : code; // if code is empty, set to empty string
-    let packageIdStrURL = 'https://makecode.calliope.cc/_X3KdiDKFifpm'; // MakeCode Project url
+    let packageIdStrURL = ''; // MakeCode Project url
     $: packageIdStr = packageIdStrURL.substring(packageIdStrURL.lastIndexOf("/") + 1); // get project id from url
     let snippetModeBool = true; // if true, remove surrounding start block
     let scalePng = 1; // faktor to scale png image
     let svgCode; 
     
+    let codePre;
+    let iframeHandler;
+    let svgContainer;
     let pngContainer; // container for png image
     let pngLink; // download link to png image
     let svgLink; // download link to svg image
 
-    $: renderSnippets(codeToRender, packageIdStr, snippetModeBool, scalePng)
+    let editorURL = "https://makecode.calliope.cc/";
+
+    $: renderSnippets(codeToRender, packageIdStr, snippetModeBool, scalePng, editorURL, code)
     
     //// MakeCode Renderer
         var makeCodeRenderPre = makeCodeRenderPre || (function () {
-        // pre waiting to be rendered
-        // when undefined, iframe is loaded and ready
-        var pendingPres = [];
-        function injectRenderer() {
-            var f = document.getElementById("makecoderenderer");
-            // check iframe already added to the DOM
-            if (f) {
-                return;
-            }
-            var f = document.createElement("iframe");
-            f.id = "makecoderenderer";
-            f.style.position = "absolute";
-            f.style.left = 0;
-            f.style.bottom = 0;
-            f.style.width = "1px";
-            f.style.height = "1px";
-            f.src = "https://makecode.calliope.cc/--docs?render=1"
-            document.body.appendChild(f);
-        }
     
-        function renderPre(pre) {
-            // console.log('render ' + pre.id)
-            var f = document.getElementById("makecoderenderer");
-            // check if iframe is added and ready (pendingPres is undefined)
-            if (!f || !!pendingPres) {
-                // queue up
-                pendingPres.push(pre);
-                injectRenderer();
-            } else {
-                f.contentWindow.postMessage({
-                    type: "renderblocks",
-                    id: pre.id,
-                    code: codeToRender,
-                    options: {
-                        packageId: packageIdStr,
-                        snippetMode: snippetModeBool
-                    }
-                }, "https://makecode.calliope.cc/");
-            }
+        function renderPre() {
+            iframeHandler.contentWindow.postMessage({
+                type: "renderblocks",
+                id: codePre.id,
+                code: codeToRender,
+                options: {
+                    packageId: packageIdStr,
+                    snippetMode: snippetModeBool
+                }
+            }, editorURL);
         }
     
         // listen for messages
@@ -144,11 +131,7 @@
             // console.log(msg.type)
             switch (msg.type) {
                 case "renderready":
-                    // flush pending requests            				
-                    var pres = pendingPres;
-                    // set as undefined to notify that iframe is ready
-                    pendingPres = undefined;
-                    pres.forEach(function (pre) { renderPre(pre); })
+                    renderPre();
                     break;
                 case "renderblocks":
                     var svg = msg.svg; // this is an string containing SVG
@@ -162,9 +145,9 @@
                     img.width = msg.width;
                     img.height = msg.height;
                     var code = document.getElementById(id);
-                    code.parentElement.querySelector('.image-container').innerHTML = '';
+                    svgContainer.innerHTML = '';
                     pngContainer.innerHTML = '';
-                    code.parentElement.querySelector('.image-container').appendChild(img);
+                    svgContainer.appendChild(img);
                     
                     svgToPng(svg,(imgData)=>{
                         const pngImage = document.createElement('img');
@@ -183,11 +166,7 @@
     
         async function renderSnippets() {
             await tick();
-        // TODO ADD RENDER LOGIC HERE
-        let pres = document.getElementsByClassName("makecodeSnippet");
-        Array.prototype.forEach.call(pres, function (pre) {
-            makeCodeRenderPre(pre);
-        })
+            makeCodeRenderPre(codePre);
         }
     //// END MakeCode Renderer
     
